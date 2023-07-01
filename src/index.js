f83f56a31717a2ee88e0574f9d12a4ddee8675e9
huyshine <huyshine1111@gmail.com>
2023-07-01 23:43:54 +0700

    update service user

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const port = process.env.PORT || 3001
 app.use(cors())
 app.use(morgan("tiny"));
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.json())
 app.use(cookieParser())
 
