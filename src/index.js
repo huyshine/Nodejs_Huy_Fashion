978fd4c277ed3429c12e25dfb958772683409243
huyshine <huyshine1111@gmail.com>
2023-07-01 22:21:48 +0700

    build sign-in/up servie

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const mongoose = require("mongoose");
 const routes = require('./routes')
 const cors = require('cors');
+const bodyParser = require('body-parser')                                                                                                                                                   
+const cookieParser = require('cookie-parser')
 
 dotenv.config()
-
 
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use(cors())
+app.use(morgan("tiny"));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 routes(app);
 
-app.use(cors())
-
+mongoose.set('strictQuery', true); 
 mongoose.connect(`${process.env.MONGO_DB}`)
     .then(() => {
-        // console.log('Connect Db success!')
+        console.log('Connect Db success!')
     })
     .catch((err) => {
         // console.log(err)
     })
 app.listen(port, () => {
-    // console.log('Server is running in port: ', + port)
+    console.log('Server is running in port: ', + port)
 })
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -5,10 +5,10 @@ const userSchema = new mongoose.Schema(
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         isAdmin: { type: Boolean, default: false, required: true },
-        phone: { type: Number },
-        address: { type: String },
-        avatar: { type: String },
-        city: {type: String}
+        phone: { type: Number }
+        // address: { type: String },
+        // avatar: { type: String },
+        // city: {type: String}
     },
     {
         timestamps: true
